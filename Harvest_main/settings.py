09c7ccdac7a89b63fa0192bc92fae9a6710ace52
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-02-14 19:20:43 +0000

    implemented crispy forms to register.html form and login form

--- Harvest_main/settings.py
+++ Harvest_main/settings.py
@@ -29,9 +29,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'account',
-   
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTH_USER_MODEL = 'account.CustomUser'
 
