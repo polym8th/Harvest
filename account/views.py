dbbb1e63a5acdd94295a874e8b418396f4cda59b
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-02-14 14:21:48 +0000

    add login page and userauthentication

--- account/views.py
+++ account/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .forms import CreateUserForm
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login 
+
 
 def home(request):
     return render(request, 'account/index.html')
@@ -19,4 +23,31 @@ def register(request):
 
 # ✅ Add the missing my_login function
 def my_login(request):
-    return render(request, 'account/my-login.html')
+    
+    form = AuthenticationForm()
+    
+    if request.method == 'POST':
+        
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None and user.is_writer==True:
+                
+                login(request, user)
+                
+                return HttpResponse('Welcome writer')
+            if user is not None and user.is_writer==False:
+                
+                login(request, user)
+                
+                return HttpResponse('Welcome reader')
+            
+    context = {'LoginForm': form}        
+    
+    return render(request, 'account/my-login.html', context)
