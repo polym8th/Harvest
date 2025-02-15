ebbfc666b91ff9f9ae5d666f81aff5d28b6779f0
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-02-15 18:57:48 +0000

    feat: Add creator's dashboard for writers and admin section for article management
    - Implement creator's dashboard for writers to manage their content

--- creator/models.py
+++ creator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from account.models import CustomUser
+
+class Article(models.Model):
+    
+    title = models.CharField(max_length=150)
+    content = models.TextField(max_length=10000)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    is_unlimited = models.BooleanField(default=False, verbose_name="Unlimited Access")
+    
+    user = models.ForeignKey(CustomUser, max_length=10, on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return self.title
 # Create your models here.
