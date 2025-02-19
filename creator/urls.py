4b8c5a90c2f83d2d4d538f72002d66236af10ab7
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-02-19 19:03:33 +0000

    feat:add update link to form in creator app

--- creator/urls.py
+++ creator/urls.py
@@ -2,13 +2,19 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
     path('creator-dashboard', views.creator_dashboard, name="creator-dashboard"),
     
     path('create-article', views.create_article, name="create-article"),
     
     path('published', views.published, name="published"),
-
+    
+    path('update-article/<str:pk>', views.update_article, name="update-article"),
+    
+    path('delete-article/<str:pk>', views.delete_article, name="delete-article"),
+    
 
     
 ]
--- creator/views.py
+++ creator/views.py
@@ -1,44 +1,67 @@
 from django.shortcuts import render, redirect
-
 from django.contrib.auth.decorators import login_required
-
-from . forms import ArticleForm
-
 from django.http import HttpResponse
-
-from . models import Article
+from .forms import ArticleForm
+from .models import Article
 
 
 @login_required(login_url='my-login')
 def creator_dashboard(request):
-    
     return render(request, 'creator/creator-dashboard.html')
 
-@login_required(login_url='login')
+
+@login_required(login_url='my-login')
 def create_article(request):
     form = ArticleForm()
 
     if request.method == 'POST':
         form = ArticleForm(request.POST)
-        
         if form.is_valid():     
             article = form.save(commit=False)
             article.user = request.user
             article.save()
-            
-            return redirect ('published')
+            return redirect('published')
 
     context = {'CreateArticleForm': form}
-
     return render(request, 'creator/create-article.html', context)
+
 
 @login_required(login_url='my-login')
 def published(request):
-    
     current_user = request.user.id
-    
-    article =  Article.objects.filter(user=current_user)
-    
-    context  = {'AllArticles': article}
-    
+    articles = Article.objects.filter(user=current_user)
+    context = {'AllArticles': articles}
     return render(request, 'creator/published.html', context)
+
+
+@login_required(login_url='my-login')
+def update_article(request, pk):
+    try:
+        article = Article.objects.get(id=pk, user=request.user)
+    except Article.DoesNotExist:
+        return redirect('published')
+
+    form = ArticleForm(instance=article)
+
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('published')
+
+    context = {'UpdateArticleForm': form}
+    return render(request, 'creator/update-article.html', context)
+
+
+@login_required(login_url='my-login')
+def delete_article(request, pk):
+    
+    article = Article.objects.get(id=pk) 
+
+    if request.method == 'POST':
+        
+        article.delete()
+        
+        return redirect('published')
+
+    return render(request, 'creator/delete-article.html')
