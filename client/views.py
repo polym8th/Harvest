a3f0f5d5c3a3b303c770ece261d83308d7a89a56
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-02-20 15:33:18 +0000

    add client dashboard

--- client/views.py
+++ client/views.py
@@ -6,3 +6,4 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='my-login')
 def client_dashboard(request):
     return render(request, 'client/client-dashboard.html') 
+  
