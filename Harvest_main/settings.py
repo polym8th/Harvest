cdce8310b09f439529b65d10cd84527697f52f43
IsaHu-dev <usunisa.hu@students.codeinstitute.net>
2025-04-07 17:34:23 +0100

    debug set to False

--- Harvest_main/settings.py
+++ Harvest_main/settings.py
@@ -16,7 +16,7 @@ cloudinary.config(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
